dc8e4f5bb6e3b20310eff105a2cda4b85771b509
wzz <49931300+wzzll123@users.noreply.github.com>
2026-05-19 10:21:28 +0800

    Clean prompt template indentation (#28)
    * Clean prompt template indentation
    
    * Add AscendC add-shot prompt CLI

--- prompt_generators/ascendc_add_shot.py
+++ prompt_generators/ascendc_add_shot.py
@@ -1,5 +1,4 @@
 from prompt_generators.prompt_registry import register_prompt, BasePromptStrategy
-from utils.utils import underscore_to_pascalcase
 from prompt_generators.prompt_utils import read_relavant_files, ascendc_template
 
 
@@ -10,3 +9,12 @@ class AscendcDefaultPromptStrategy(BasePromptStrategy):
         arc_src, example_arch_src, example_new_arch_src = read_relavant_files('ascendc', op, 'add')
         return ascendc_template(arc_src, example_arch_src, example_new_arch_src, op, 'add')
 
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Print the AscendC add-shot prompt for an op.")
+    parser.add_argument("op", nargs="?", default="relu", help="Benchmark op name, default: relu")
+    args = parser.parse_args()
+
+    print(AscendcDefaultPromptStrategy().generate(args.op))
